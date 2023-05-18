83a60612d3302a69381cf041ad408769fac7059e
pnina-miller <pnina-miller>
2023-05-19 01:38:55 +0300

    try static

---
+++ precache-manifest.4493687306b5b79955af6297eb73ad6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2538df147e602cd2003d2f27efc328d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2897a052b2096dac6b4d",
+    "url": "/static/css/main.1bb5d81a.chunk.css"
+  },
+  {
+    "revision": "516c7662faf3e094f7ed",
+    "url": "/static/js/2.e346df47.chunk.js"
+  },
+  {
+    "revision": "251ed0272d3a5bacbf66c7e872235080",
+    "url": "/static/js/2.e346df47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2897a052b2096dac6b4d",
+    "url": "/static/js/main.efd99333.chunk.js"
+  },
+  {
+    "revision": "316d989000b7f3ff2333",
+    "url": "/static/js/runtime-main.da5c926e.js"
+  },
+  {
+    "revision": "1f630bd1dcd87274a95767237901812d",
+    "url": "/static/media/bicycle.1f630bd1.png"
+  },
+  {
+    "revision": "1087d69d8c5e1ee7a3d56fc35184ea3e",
+    "url": "/static/media/plus-circle.1087d69d.svg"
+  },
+  {
+    "revision": "01577e5dfbe0aaca934d4fc2ad6d40b0",
+    "url": "/static/media/search.01577e5d.svg"
+  }
+]);
