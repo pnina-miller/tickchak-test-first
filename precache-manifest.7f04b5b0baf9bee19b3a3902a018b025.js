f431a7fbfad5ab9de4877c7fd3eb078b5fd1896d
pnina-miller <pnina-miller>
2023-05-19 01:48:22 +0300

    Updates

---
+++ precache-manifest.7f04b5b0baf9bee19b3a3902a018b025.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985e8a10183446be7c9036196dd54621",
+    "url": "/pnina-miller/tickchak-testindex.html"
+  },
+  {
+    "revision": "62c98f49c5ee0c78cfa6",
+    "url": "/pnina-miller/tickchak-teststatic/css/main.cc6e32d2.chunk.css"
+  },
+  {
+    "revision": "be98713a9faf69b6b977",
+    "url": "/pnina-miller/tickchak-teststatic/js/2.0be963ae.chunk.js"
+  },
+  {
+    "revision": "251ed0272d3a5bacbf66c7e872235080",
+    "url": "/pnina-miller/tickchak-teststatic/js/2.0be963ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c98f49c5ee0c78cfa6",
+    "url": "/pnina-miller/tickchak-teststatic/js/main.efd99333.chunk.js"
+  },
+  {
+    "revision": "03b414444e50f4c8c76f",
+    "url": "/pnina-miller/tickchak-teststatic/js/runtime-main.7fc3096f.js"
+  },
+  {
+    "revision": "1f630bd1dcd87274a95767237901812d",
+    "url": "/pnina-miller/tickchak-teststatic/media/bicycle.1f630bd1.png"
+  },
+  {
+    "revision": "1087d69d8c5e1ee7a3d56fc35184ea3e",
+    "url": "/pnina-miller/tickchak-teststatic/media/plus-circle.1087d69d.svg"
+  },
+  {
+    "revision": "01577e5dfbe0aaca934d4fc2ad6d40b0",
+    "url": "/pnina-miller/tickchak-teststatic/media/search.01577e5d.svg"
+  }
+]);
